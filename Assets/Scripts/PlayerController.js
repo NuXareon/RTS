c691eb1a7b436f546c8aa317f61d8d4580deb199
NuXareon <millas@gmail.com>
2014-07-10 13:50:38 +0200

    RTS - 3

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -1,6 +1,10 @@
 ﻿#pragma strict
 
 private var selected : GameObject;
+private var selectionMode : boolean;
+private var iniSelection : Vector3;
+private var finalSelection : Vector3;
+private var selectedArr : GameObject[];
 
 var cam : Camera;
 
@@ -10,20 +14,83 @@ function OnGUI () {
 
 function Start () {
 	selected = gameObject;
+	selectionMode = false;
+	var units : GameObject[] = GameObject.FindGameObjectsWithTag("Unit");
+	selectedArr = new GameObject[units.Length];
 }
 
 function Update () {
 	var ray : Ray;
 	var hit : RaycastHit;
 	var isHit : boolean;
+	var i : int;
 	if (Input.GetButtonDown("Fire1")) {
+		for (i = 0; i < selectedArr.Length;++i) selectedArr[i] = null;
 		ray = cam.ScreenPointToRay (Input.mousePosition);
-		isHit = Physics.Raycast(ray,hit,1000,1<<8);
+		isHit = Physics.Raycast(ray,hit,1000,1<<8|1<<9);
 		if (isHit) {
-			selected = hit.collider.gameObject;
+			if (hit.collider.gameObject.layer == 8) {
+				selected = hit.collider.gameObject;
+				selectionMode = false;
+			} else {
+				iniSelection = hit.point;
+				selectionMode = true;
+			}
 		}
 	}
-	if (Input.GetButton("Fire2")) {
+	if (Input.GetButtonUp("Fire1") && selectionMode) {
+		selectionMode = false;
+		var units : GameObject[] = GameObject.FindGameObjectsWithTag("Unit");
+		var selectionCase : int;
+		if (iniSelection.x < finalSelection.x){
+			if (iniSelection.z < finalSelection.z) selectionCase = 0;
+			if (iniSelection.z >= finalSelection.z) selectionCase = 1;
+		} else if (iniSelection.x >= finalSelection.x) {
+			if (iniSelection.z < finalSelection.z) selectionCase = 2;
+			if (iniSelection.z >= finalSelection.z) selectionCase = 3;
+		}
+		var pos : int = 0;
+		for (var unit : GameObject in units) {
+			var unitPos : Vector3 = unit.transform.position;
+			if (selectionCase == 0) {
+				if (unitPos.x >= iniSelection.x && unitPos.x <= finalSelection.x &&
+					unitPos.z >= iniSelection.z && unitPos.z <= finalSelection.z) {
+						selectedArr[pos] = unit;
+						pos++;
+					}
+			}
+			if (selectionCase == 1) {
+				if (unitPos.x >= iniSelection.x && unitPos.x <= finalSelection.x &&
+					unitPos.z <= iniSelection.z && unitPos.z >= finalSelection.z) {
+						selectedArr[pos] = unit;
+						pos++;
+					}
+			}
+			if (selectionCase == 2) {
+				if (unitPos.x <= iniSelection.x && unitPos.x >= finalSelection.x &&
+					unitPos.z >= iniSelection.z && unitPos.z <= finalSelection.z) {
+						selectedArr[pos] = unit;
+						pos++;
+					}
+			}
+			if (selectionCase == 3) {
+				if (unitPos.x <= iniSelection.x && unitPos.x >= finalSelection.x &&
+					unitPos.z <= iniSelection.z && unitPos.z >= finalSelection.z) {
+						selectedArr[pos] = unit;
+						pos++;
+					}
+			}
+		}
+		for (i = 0; i < selectedArr.Length;++i) {
+			print(selectedArr[i]);
+		}
+	}
+	if (Input.GetButton("Fire1") && selectionMode) {
+		ray = cam.ScreenPointToRay (Input.mousePosition);
+		Physics.Raycast(ray,hit,1000,1<<9);
+		finalSelection = hit.point;
+	}
+	else if (Input.GetButton("Fire2")) {
 		if (selected.layer == 8) {
 			var mask = 1<<9|1<<10;
 			ray = cam.ScreenPointToRay (Input.mousePosition);
