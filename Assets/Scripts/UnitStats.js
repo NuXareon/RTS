86776e83b4a1ce22e1ea1e3a6780b5a4fdc82545
NuXareon <millas@gmail.com>
2014-07-11 00:00:54 +0200

    RTS - 5

--- Assets/Scripts/UnitStats.js
+++ Assets/Scripts/UnitStats.js
@@ -1,25 +1,38 @@
 ﻿#pragma strict
 
-var HP : int;
+var maxHP : int;
 var AttackRange : float;
 var AttackDamage : int;
 var AttackSpeed : float;
+var HealthBarStyle : GUIStyle;
+var HealthBarStyleHP : GUIStyle;
+var HealthBarStyleBG : GUIStyle;
 
+private var HP : int;
 private var AttackCD : float;
 private var Attacking : boolean;
 private var Target : GameObject;
 private var Selected : boolean;
 private var SelectRenderer : Renderer;
+private var RangeRenderer : Renderer;
 
 function OnGUI() {
+	var targetScreenPos = Camera.main.WorldToScreenPoint (transform.position);
+	var targetScreenPosUp = Camera.main.WorldToScreenPoint (transform.position+Vector3(0,15,0));
+	GUI.Box(Rect(targetScreenPos.x-20,Screen.height-targetScreenPosUp.y,40,0),"",HealthBarStyleBG);
+	GUI.Box(Rect(targetScreenPos.x-20,Screen.height-targetScreenPosUp.y,40,0),"",HealthBarStyle);
+	GUI.Box(Rect(targetScreenPos.x-20,Screen.height-targetScreenPosUp.y,40*HP/maxHP,0),"",HealthBarStyleHP);
 	// Print HP bar;
 }
 
 function Start () {
+	HP = maxHP;
 	Attacking = false;
 	AttackCD = 0.0f;
 	Selected = false;
 	SelectRenderer = transform.Find("Selected").GetComponent(Renderer);
+	RangeRenderer = transform.Find("Range").GetComponent(Renderer);
+	
 }
 
 function Update () {
@@ -36,7 +49,8 @@ function Update () {
 		AttackCD = AttackSpeed;
 		}
 	}
-	SelectRenderer.enabled = Selected;	
+	SelectRenderer.enabled = Selected;
+	RangeRenderer.enabled = Selected;
 }
 
 function DecreaseHP(amount : int) {
